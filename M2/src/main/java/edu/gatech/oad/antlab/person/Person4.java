26676f031763ef8b212a82fbffabf8bec822bd5a
jnguyen5 <MieNguyen@lawn-143-215-121-160.lawn.gatech.edu>
2018-01-26 13:32:44 -0500

    changed perosn 4 file

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+        String returnName = "";
+        for (int i = 0; i < input.length(); i++) {
+            returnName = returnName + ("" + input.charAt(i) + 1);
+        }
+        return returnName;
     }
     
     /**
