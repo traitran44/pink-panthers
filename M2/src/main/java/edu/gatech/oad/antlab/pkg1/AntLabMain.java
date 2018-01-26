0c91dfed17a1e6025f6e5264908b428e3a674d62
jeannie3 <jeannie3@gatech.edu>
2018-01-26 16:34:52 -0500

    Edited AntLabMain for person 3

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,7 +66,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Jeannie Le");
-		  toPrint += p3.toString("jeannie3");
+		  toPrint += p3.toString("903220190");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
