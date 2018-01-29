adb6e9b76b9fc30048a489e2558987b29986a31a
jnguyen5 <MieNguyen@lawn-143-215-114-101.lawn.gatech.edu>
2018-01-29 12:03:12 -0500

    made changes to person 4 file

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -27,15 +27,15 @@ public class Person4 {
      * "bcd234".
      *
      * @param input the string to be modified
-     * @return the modified string
+     * @return newName the modified string
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-        String returnName = "";
+        String newName = "";
         for (int i = 0; i < input.length(); i++) {
-            returnName = returnName + ("" + input.charAt(i) + 1);
+            newName = newName + (char)("" + input.charAt(i) + 1);
         }
-        return returnName;
+        return newName;
     }
     
     /**
