fa3bd59848e894e961e1acaade8ffc129b49d1de
Your Nameman <jnguyen5@gatech.edu>
2018-01-30 23:08:54 -0500

    made changes to person 4

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -76,8 +76,8 @@ public class AntLabMain {
         toPrint += p3.toString("jeannie3");
         //Person4 replace P4 with your name
         //and gburdell4 with your gt id
-        Person4 p4 = new Person4("P4");
-        toPrint += p4.toString("gburdell4");
+        Person4 p4 = new Person4("My Nguyen");
+        toPrint += p4.toString("mnguyen300");
         //Person5 replace P4 with your name
         //and gburdell5 with your gt id
         Person5 p5 = new Person5("Trai Tran");
