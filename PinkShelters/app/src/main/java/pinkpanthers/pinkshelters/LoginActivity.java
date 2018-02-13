eeea7e3fedb956d5978b055d0f8b99e419d8bd24
Hao Dang <hdang32@gatech.edu>
2018-02-12 23:26:39 -0500

    add PinkShelters files

---
+++ PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/LoginActivity.java
@@ -0,0 +1,20 @@
+package pinkpanthers.pinkshelters;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void logIn(View view) {
+        Intent homePageIntent = new Intent(this, HomePageActivity.class);
+        startActivity(homePageIntent);
+    }
+}
