43bd37d8641f2a50474a56d842da49425250982d
Your Nameman <jnguyen5@gatech.edu>
2018-02-14 00:49:34 -0500

    when press logout button, it exits out

--- PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/HomePageActivity.java
+++ PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/HomePageActivity.java
@@ -2,10 +2,22 @@ package pinkpanthers.pinkshelters;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 import android.widget.Button;
+import android.view.View;
 
 public class HomePageActivity extends AppCompatActivity {
     private Button log_out_button;
+
+    public void buttonOnClick(View v) {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+        finish();
+        Button log_out_button=(Button) v;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,3 +25,4 @@ public class HomePageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
     }
 }
+
