d558db1105ee79ccc4cfc3018917ff2d4c1d436f
jeannie3 <jeannie3@gatech.edu>
2018-03-13 22:06:22 -0400

    Merge branch 'master' of https://github.com/MrTrai/pink-panthers into jeannie-le-person3
    # Conflicts:
    #	PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/HomePageActivity.java
    #	PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/LoginActivity.java
    #	PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/UserInfoActivity.java

--- PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/ShelterDetails.java
+++ PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/ShelterDetails.java
@@ -1,18 +1,14 @@
 package pinkpanthers.pinkshelters;
 
-
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -29,6 +25,7 @@ public class ShelterDetails extends AppCompatActivity {
     private Button updateInfoButton;
     private Button cancelBedButton;
     private String username;
+    private String message; //message for error message
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,11 +56,16 @@ public class ShelterDetails extends AppCompatActivity {
                 claimBedButton.setVisibility(View.VISIBLE);
                 try {
                     reservedShelter = db.getShelterById(a.getShelterId());
-                    if (reservedShelter.equals(s)) {
+                    if (reservedShelter.getId() == s.getId()) {
                         cancelBedButton.setVisibility(View.VISIBLE);
+                        message = "You have claimed " + a.getFamilyMemberNumber()
+                                + " bed(s) at this shelter";
+                        errorMessage.setText(message);
+                        errorMessage.setTextColor(Color.GREEN);
+                        errorMessage.setVisibility(View.VISIBLE);
                     }
                 } catch (NoSuchUserException e) {
-                    // this shelter is not the one the user reserved to
+                    // this shelter is not the one that the user reserved to
                     // so nothing happens (cancel reservation button remains invisible)
                 }
             } else { // user is not a homeless person
@@ -116,66 +118,83 @@ public class ShelterDetails extends AppCompatActivity {
     public void claimBedButton(View view) {
         // check to see if user has updated their information
         if (a.getFamilyMemberNumber() == 0 || a.getRestrictionsMatch() == null) {
-            errorMessage.setText("You need to update your information before you can claim a bed or beds. " +
-                    "Please update your information");
+            message = "You need to update your information before you can claim a bed or beds. "
+                    + "Please update your information by using the button below";
+            errorMessage.setText(message);
             errorMessage.setVisibility(View.VISIBLE);
             updateInfoButton.setVisibility(View.VISIBLE);
         } else { //homeless person cant claim bed(s) if they have already claimed bed(s) at a different shelter
             int familyMemberNumber = a.getFamilyMemberNumber();
             if (a.getShelterId() != 0) {
-                errorMessage.setText("Sorry, you have already claimed " + familyMemberNumber + "bed(s)");
+                if (s.getId() == reservedShelter.getId()) {
+                    message = "Sorry, you have already claimed " + familyMemberNumber +
+                            " bed(s)";
+                    errorMessage.setText(message);
+                    errorMessage.setTextColor(Color.RED);
+                } else {
+                    message = "You have already claimed " + familyMemberNumber +
+                            " bed(s) at another shelter called " + reservedShelter.getShelterName();
+                    errorMessage.setText(message);
+                    errorMessage.setTextColor(Color.RED);
+                }
                 errorMessage.setVisibility(View.VISIBLE);
+
                 // check if there are still available bed(s) for their families
             } else if (s.getVacancy() <= familyMemberNumber) {
-                errorMessage.setText("Sorry, there are not enough beds");
+                message = "Sorry, there are not enough beds";
+                errorMessage.setText(message);
                 errorMessage.setVisibility(View.VISIBLE);
 
             } else {
-                String anyone = new String("anyone ");
-                Boolean pass;
                 //set of restriction of shelter
-                Set < String > shelterRestrictionSet = new HashSet < > ();
+                Set <String> shelterRestrictionSet = new HashSet <> ();
+
                 //turn String of shelter restriction to List
-                List < String > shelterRestrictionList = Arrays.asList(s.getRestrictions().split((",")));
+                List <String> shelterRestrictionList = Arrays.asList(s.getRestrictions().split((",")));
+
                 //add Strings of shelter restriction list to set
                 for (String s: shelterRestrictionList) {
                     shelterRestrictionSet.add(s.toLowerCase());
                 }
+
                 //list of homeless restriction
                 List < String > homelessRestrictions = a.getRestrictionsMatch();
+
                 //turn list into set of homeless restriction
-                Set < String > homelessRestrictionsSet = new HashSet < > (homelessRestrictions);
-                Set < String > common = new HashSet < > (shelterRestrictionSet);
+                Set <String> homelessRestrictionsSet = new HashSet <> (homelessRestrictions);
+                Set <String> common = new HashSet <> (shelterRestrictionSet);
                 common.retainAll(homelessRestrictionsSet);
-                String anyone1 = s.getRestrictions().toLowerCase().toString();
-                if (anyone1.equals(anyone) || (common.equals(homelessRestrictionsSet))) {
-                    pass = true;
+                String anyone1 = s.getRestrictions().toLowerCase();
+                if (anyone1.equals("anyone") || (common.equals(homelessRestrictionsSet))) {
                     try {
-                        db = new Db("pinkpanther", "PinkPantherReturns!", "pinkpanther");
                         //update vacancy of shelter
                         int vacancy1 = s.getVacancy() - familyMemberNumber;
                         s.setVacancy(vacancy1);
+
                         //update current vacancy on screen
                         String forVacancy = "Vacancy: " + s.getVacancy();
                         vacancy.setText(forVacancy);
+
                         //update shelter occupancy for Db
                         int occupancy = s.getUpdate_capacity() - vacancy1;
                         s.setOccupancy(occupancy);
                         db.updateShelterOccupancy(s.getId(), occupancy);
                         a.setShelterId(s.getId());
+
                         //pass in account object to update account
                         db.updateAccount(a);
-                        String success = "You have claimed " + familyMemberNumber + " bed(s) successfully";
-                        errorMessage.setText(success);
-                        errorMessage.setVisibility(View.VISIBLE);
-                        errorMessage.setTextColor(Color.GREEN);
+
+                        // refresh page to update vacancy TextView
+                        finish();
+                        startActivity(getIntent());
                     } catch (SQLException e) {
                         e.printStackTrace();
                     } catch (NoSuchUserException e) {
                         e.printStackTrace();
                     }
                 } else {
-                    errorMessage.setText("You do not fit the restrictions of this shelter");
+                    message = "You do not fit the restrictions of this shelter";
+                    errorMessage.setText(message);
                     errorMessage.setVisibility(View.VISIBLE);
                 }
             }
@@ -190,22 +209,37 @@ public class ShelterDetails extends AppCompatActivity {
     }
 
     public void cancelReservationButton(View view) {
-        s.setVacancy(s.getVacancy() + a.getFamilyMemberNumber());
-        String forVacancy = "Vacancy: " + s.getVacancy();
+        // update vacany
+        int vacancy1 = s.getVacancy() + a.getFamilyMemberNumber();
+        s.setVacancy(vacancy1);
+        String forVacancy = "Vacancy: " + vacancy1;
         vacancy.setText(forVacancy);
-        int occupancy = s.getUpdate_capacity() - s.getVacancy();
+
+        //update occupancy
+        int occupancy = s.getUpdate_capacity() - vacancy1;
         s.setOccupancy(occupancy);
+
+        //update homeless's shelter id
         a.setShelterId(0); // no longer associate with any shelter
         try {
             db.updateAccount(a);
             db.updateShelterOccupancy(s.getId(), occupancy);
         } catch (NoSuchUserException e) {
-            throw new RuntimeException("Homeless user is null");
+            throw new RuntimeException("Homeless user is null or shelterid does not exist");
         } catch (java.sql.SQLException e) {
-            throw new RuntimeException("SQLException raised when trying to update account" +
+            throw new RuntimeException("SQLException raised when trying to update account or shelter" +
                     " during canceling reservation");
         }
 
+        message = "You have successfully cancel your reservation for " +
+                a.getFamilyMemberNumber() + " bed(s)";
+        errorMessage.setText(message);
+        errorMessage.setTextColor(Color.GREEN);
+        errorMessage.setVisibility(View.VISIBLE);
+
+        // refresh page to update vacancy textView
+        finish();
+        startActivity(getIntent());
     }
 
 }
