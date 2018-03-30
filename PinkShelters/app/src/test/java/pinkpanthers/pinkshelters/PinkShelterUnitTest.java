70ab0c2b9526b1725e0358a9494f39e6f7302b75
Trai Tran <ttran96@gatech.edu>
2018-03-30 13:08:51 -0400

    unit test file

--- PinkShelters/app/src/test/java/pinkpanthers/pinkshelters/PinkShelterUnitTest.java
+++ PinkShelters/app/src/test/java/pinkpanthers/pinkshelters/PinkShelterUnitTest.java
@@ -1,8 +1,14 @@
 package pinkpanthers.pinkshelters;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by Trai Tran on 3/30/2018.
  */
 
 public class PinkShelterUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
 }
