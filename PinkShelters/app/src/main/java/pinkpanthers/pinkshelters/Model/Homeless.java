bc1a1c5a946215d1d5cdc21d4f4b8adcc1e46ff9
Hao Dang <hdang32@gatech.edu>
2018-03-31 23:35:03 -0400

    add javadoc for Homeless

--- PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/Model/Homeless.java
+++ PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/Model/Homeless.java
@@ -2,11 +2,24 @@ package pinkpanthers.pinkshelters.Model;
 
 import java.util.List;
 
+/**
+ * Homeless class, one of 3 user type
+ */
 public class Homeless extends Account {
     private int shelterId; // might change to int depends on the type parsed in csv file
     private List<String> restrictionsMatch;
     private int familyMemberNumber;
 
+    /**
+     * constructor
+     *
+     * @param username     username of account
+     * @param password     password of account
+     * @param name         name of owner of account
+     * @param accountState blocked or active
+     * @param email        email address of account
+     * @param userId       user id of account in database
+     */
     public Homeless(String username,
                     String password,
                     String name,
@@ -16,27 +29,58 @@ public class Homeless extends Account {
         super(username, password, name, accountState, email, userId);
     }
 
-    // setter/getter deals with homeless check-in/out a shelter
+    /**
+     * set new shelterId
+     *
+     * @param shelterId id of a shelter in database
+     */
     public void setShelterId(int shelterId) {
         this.shelterId = shelterId;
     }
 
+    /**
+     * get shelterId
+     *
+     * @return shelter id that the current homeless checks in
+     */
     public int getShelterId() {
         return this.shelterId;
     }
 
+    /**
+     * set new total family member number
+     *
+     * @param familyMemberNumber number of family members including self
+     */
     public void setFamilyMemberNumber(int familyMemberNumber) {
         this.familyMemberNumber = familyMemberNumber;
     }
 
+    /**
+     * get current total family member number
+     *
+     * @return number of family members including self
+     */
     public int getFamilyMemberNumber() {
         return this.familyMemberNumber;
     }
 
+    /**
+     * set new restriction match
+     *
+     * @param restrictionsMatch list of restrictions that current homeless
+     *                          matches with shelter
+     */
     public void setRestrictionsMatch(List<String> restrictionsMatch) {
         this.restrictionsMatch = restrictionsMatch;
     }
 
+    /**
+     * get current list of restriction match
+     *
+     * @return list of restrictions that current homeless
+     * matches with shelter
+     */
     public List<String> getRestrictionsMatch() {
         return this.restrictionsMatch;
     }
