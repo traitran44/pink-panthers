9a7ad2ffcc16407db728de0ec27f932f68cbad2b
Hao Dang <hdang32@gatech.edu>
2018-03-31 23:24:16 -0400

    add javadoc for Account

--- PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/Model/Account.java
+++ PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/Model/Account.java
@@ -9,6 +9,16 @@ public abstract class Account {
     private String name;
     private int userId;
 
+    /**
+     * constructor
+     *
+     * @param username     username of account
+     * @param password     password of account
+     * @param name         name of owner of account
+     * @param accountState active or blocked
+     * @param email        email address
+     * @param userId       unique identity created by the database
+     */
     Account(String username,
             String password,
             String name,
@@ -23,50 +33,99 @@ public abstract class Account {
         this.userId = userId;
     }
 
+    /**
+     * set new username
+     * @param username username of account
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * get current username
+     * @return username of account
+     */
     public String getUsername() {
         return this.username;
     }
 
+    /**
+     * set new password
+     * @param password new password of account
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+
+    /**
+     * get password of account
+     * @return current password of account
+     */
     public String getPassword() {
         return this.password;
     }
 
+    /**
+     * set new name of owner of account
+     * @param name new name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * get name of the owner of account
+     * @return current name of the owner
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * set account state to blocked or active
+     * @param accountState blocked or active
+     */
     public void setAccountState(String accountState) {
         this.accountState = accountState;
     }
 
+    /**
+     * get account state
+     * @return current account state
+     */
     public String getAccountState() {
         return this.accountState;
     }
 
+    /**
+     * set new email address
+     * @param email new address
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * get current email address
+     * @return current email address
+     */
     public String getEmail() {
         return this.email;
     }
 
+    /**
+     * set userId of the account
+     * @param userId user identification
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
+    /**
+     * get current user identification of the account
+     * @return the current user id
+     */
     public int getUserId() {
         return this.userId;
     }
