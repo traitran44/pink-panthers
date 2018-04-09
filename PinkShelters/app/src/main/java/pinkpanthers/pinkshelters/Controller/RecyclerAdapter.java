f39f7edde905a0567cf62b09e456187ac1915b3b
Julia <jnguyen5@gatech.edu>
2018-04-09 14:24:23 -0400

    deleted extra unused imports

--- PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/Controller/RecyclerAdapter.java
+++ PinkShelters/app/src/main/java/pinkpanthers/pinkshelters/Controller/RecyclerAdapter.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.Collections;
 import java.util.List;
 
 import pinkpanthers.pinkshelters.R;
